3426128ebd8cbbc3094cd3d9940f80e0b33fa241
Timothy J. Wood <tjw@omnigroup.com>
2013-01-02 14:05:46 -0800

    Starting on influence particles. But, will try converting to SceneKit for display first since particle systems will be a bunch of effort in AppKit and will result in tons of views/constraints.

---
+++ Source/Model/ParticleSystem.h
@@ -0,0 +1,13 @@
+//
+//  Particle.h
+//  Grid
+//
+//  Created by Timothy J. Wood on 1/2/13.
+//  Copyright (c) 2013 Cocoatoa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Particle : NSObject
+
+@end
---
+++ Source/Model/ParticleSystem.m
@@ -0,0 +1,13 @@
+//
+//  Particle.m
+//  Grid
+//
+//  Created by Timothy J. Wood on 1/2/13.
+//  Copyright (c) 2013 Cocoatoa. All rights reserved.
+//
+
+#import "Particle.h"
+
+@implementation Particle
+
+@end
